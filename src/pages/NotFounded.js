33c18c3d55ef80fa5ffa604348849637ad64c982
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-04-23 11:13:43 -0500

    Main & 404 pages created with react-dom-router

---
+++ src/pages/NotFounded.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFounded = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>Page not founded</h2>
+            Go back <a href="/">home</a>
+        </div>
+    )
+}
+
+export default NotFounded
