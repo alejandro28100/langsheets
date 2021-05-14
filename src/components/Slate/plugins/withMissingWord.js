9fac8e06b0dc97b99511035756c1f326c2362fce
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-05-14 13:20:54 -0500

    Add validation to have dynamic 'paragraph-with-missign-word' block element

--- src/components/Slate/plugins/withMissingWord.js
+++ src/components/Slate/plugins/withMissingWord.js
@@ -1,4 +1,4 @@
-import { Node, Element, Transforms, Path } from 'slate';
+import { Node, Element, Transforms, Path, Editor } from 'slate';
 
 const withMissingWord = (editor) => {
 
@@ -6,18 +6,45 @@ const withMissingWord = (editor) => {
 
     editor.normalizeNode = entry => {
         const [node, path] = entry;
+
+        //Check whethert the block is a paragraph or a paragraph-with-missing-word
+        if (Editor.isBlock(editor, node) && ["paragraph", "paragraph-with-missing-word"].includes(node.type)) {
+
+            let hasMissingWords = false;
+
+            //Loock for missing word items inside the paragraph
+            for (const [child] of Node.descendants(node)) {
+                if (child.missingWord) {
+                    hasMissingWords = true;
+                }
+            }
+
+            //Make sure the type of paragraph corresponds 
+            //to whether or nor they have a missing word item inside of them
+
+            if (hasMissingWords) {
+                if (node.type !== "paragraph-with-missing-word") {
+                    return Transforms.setNodes(editor, { type: "paragraph-with-missing-word" }, { at: path });
+                };
+            } else {
+                if (node.type === "paragraph-with-missing-word") {
+                    return Transforms.setNodes(editor, { type: "paragraph" }, { at: path });
+                }
+            }
+
+        }
+
         if (Node.isNode(node) && Element.isElement(node)) {
             const firstNodePath = Node.first(editor, path)[1];
             const lastNodePath = Node.last(editor, path)[1];
 
-            // console.log(firstNode, lastNode);
             for (const [child, childPath] of Node.children(editor, path)) {
 
                 if (child.missingWord === true) {
                     // If the node is the last child, 
                     // insert child with an space after it
                     if (Path.equals(childPath, lastNodePath)) {
-                        Transforms.insertNodes(editor, { text: " " }, {
+                        return Transforms.insertNodes(editor, { text: " " }, {
                             at: Path.next(childPath),
                             select: true,
                             hanging: true
@@ -26,7 +53,7 @@ const withMissingWord = (editor) => {
 
                     // If the node is the first child, insert an empty node before
                     if (Path.equals(childPath, firstNodePath)) {
-                        Transforms.insertNodes(editor, { text: ' ' },
+                        return Transforms.insertNodes(editor, { text: ' ' },
                             {
                                 at: childPath,
                                 select: true,
@@ -39,7 +66,7 @@ const withMissingWord = (editor) => {
                     if (Node.has(editor, Path.next(childPath))) {
                         let { text } = Node.get(editor, Path.next(childPath));
                         if (text[0] !== ' ') {
-                            Transforms.insertText(editor, ' ', {
+                            return Transforms.insertText(editor, ' ', {
                                 at: { path: Path.next(childPath), offset: 0 }
                             });
                         }
