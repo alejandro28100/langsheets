7303a2d6627d28d3377c5053c411ebdc99ae1986
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-05-17 09:05:52 -0500

    Add custom paragraphs for word-order & missing-word exercises

--- src/hooks/useSlateRender.js
+++ src/hooks/useSlateRender.js
@@ -2,6 +2,8 @@ import React, { useCallback } from 'react'
 //Slate elements
 import Leaf from "../components/Slate/Inline/Leaf";
 import DefaultElement from "../components/Slate/Block/DefaultElement";
+import MissingWordExerciseParagraph from "../components/Slate/Block/MissingWordExerciseParagraph";
+import WordOrderExerciseParagraph from "../components/Slate/Block/WordOrderExerciseParagraph";
 import Heading from "../components/Slate/Block/Heading";
 import { ExerciseList, ExerciseBlock } from '../components/Slate/Block/ExerciseBlock';
 
@@ -23,6 +25,12 @@ const useSlateRender = () => {
                 return <ExerciseBlock {...props} />
             case 'exercise-list-items':
                 return <ExerciseList {...props} />
+            case 'missing-word-exercise-paragraph':
+                return <MissingWordExerciseParagraph {...props} />
+
+            case 'word-order-exercise-paragraph':
+                return <WordOrderExerciseParagraph {...props} />
+
             default:
                 return <DefaultElement {...props} />
         }
