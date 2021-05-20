28c2a682a9a0fa7659c78e5c3d5a348df384b07c
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-05-19 22:03:38 -0500

    Add custom theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
 
 import Home from "./pages/Home";
 import Form from "./pages/Form";
 import Practice from "./pages/Practice";
 import NotFounded from "./pages/NotFounded";
 
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Router>
           <Switch>
             <Route path="/" exact component={Home} />
---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+    colors: {
+        brand: {
+            50: "#eaeaff",
+            100: "#c2c2f3",
+            200: "#9b9ae6",
+            300: "#7371da",
+            400: "#4c48cf",
+            500: "#332fb6",
+            600: "#27258e",
+            700: "#1b1a66",
+            800: "#0f1040",
+            900: "#06041a",
+        }
+    }
+})
+
+export default theme;
