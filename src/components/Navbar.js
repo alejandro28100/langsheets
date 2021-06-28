671d6b598d4b6518f4e8eea356bc450039c2a093
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-06-27 20:43:21 -0500

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react'
 import PropTypes from "prop-types";
-import { Flex, Spacer, Box, Icon, Menu, MenuList, MenuButton, Button, useMediaQuery, IconButton, Text, MenuItem } from "@chakra-ui/react"
+import { Flex, Spacer, Box, Icon, Menu, MenuList, MenuButton, Button, useMediaQuery, IconButton, Text, MenuItem, MenuDivider } from "@chakra-ui/react"
 import { FaUserCircle } from "react-icons/fa"
 import { HiMenuAlt4 } from "react-icons/hi";
 import { FiLogOut } from "react-icons/fi"
-import Logo from "./Logo";
+import { Logo } from "../svgs/index";
 
 import { useUser } from "../context/UserContext";
 
 function Navbar({ leftActions, rightActions, withIcon, sm }) {
     const [isTabletOrLower] = useMediaQuery(["(max-width:900px)"]);
-    const { logout, user } = useUser();
+    const { logout, user, loading } = useUser();
 
     return (
         <Flex px="10" background="white" py="4" shadow="sm" alignItems="center"
@@ -20,16 +20,32 @@ function Navbar({ leftActions, rightActions, withIcon, sm }) {
                 }
             }}
         >
-            <Logo {...withIcon} />
+            <Button as="a" href="/" variant="unstyled">
+                <Icon w={10} h={10} as={Logo} />
+            </Button>
 
             {
                 isTabletOrLower
                     ? (
-                        sm && <Fragment>
+                        <Fragment>
                             <Spacer />
                             <Menu>
-                                <MenuButton colorScheme="purple" variant="ghost" as={Button} children={<Icon as={HiMenuAlt4} />} />
+                                <MenuButton colorScheme="brand" variant="ghost" as={Button} children={<Icon as={HiMenuAlt4} />} />
                                 <MenuList zIndex="dropdown">
+                                    {
+                                        !loading && (
+                                            <Fragment>
+                                                <Text p="4" fontSize="x-large" fontWeight="medium">
+                                                    {`${user.name} ${user.lastName}`}
+                                                </Text>
+                                                <MenuItem onClick={logout} icon={<Icon as={FiLogOut} />} >
+                                                    Cerrar Sesión
+                                                </MenuItem>
+                                            </Fragment>
+                                        )
+                                    }
+
+                                    {!!sm && <MenuDivider />}
                                     {sm}
                                 </MenuList>
                             </Menu>
@@ -43,7 +59,7 @@ function Navbar({ leftActions, rightActions, withIcon, sm }) {
 
                             {user && <Box>
                                 <Menu>
-                                    <MenuButton colorScheme="brand" as={IconButton} icon={<Icon w={8} h={8} as={FaUserCircle} />} variant="ghost" />
+                                    <MenuButton variant="ghost" colorScheme="brand" as={IconButton} icon={<Icon w={8} h={8} as={FaUserCircle} />} />
 
                                     <MenuList>
                                         <Box p="5">
