15d6bf6b600b8e030f5c9d1ec585780b37777fe0
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-06-28 00:24:55 -0500

    Enable real-time answers for WordOrder exercise

--- src/components/Slate/Block/WordOrderExerciseParagraph.js
+++ src/components/Slate/Block/WordOrderExerciseParagraph.js
@@ -1,11 +1,14 @@
-import React, { useRef } from 'react'
+import React, { useContext, useRef } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { ReactEditor, useReadOnly, useSlate } from 'slate-react';
-
-import { Flex } from '@chakra-ui/layout';
-import { Text } from '@chakra-ui/layout';
 import { Node, Transforms } from 'slate';
+
+import { Flex, Text } from '@chakra-ui/layout';
+import { Tooltip } from "@chakra-ui/react";
+import UserLabel from '../Inline/MissingWord/UserLabel';
+
 import { getDivisions, getExerciseBlockProps } from '../../../utils/exerciseBlocks';
+import { SocketContext } from '../../../context/socket';
 
 const WordOrderExerciseParagraph = (props) => {
     const isReadOnly = useReadOnly();
@@ -17,10 +20,16 @@ const WordOrderExerciseParagraph = (props) => {
 }
 
 const WordOrderPractice = props => {
+
+    const socket = useContext(SocketContext);
+
     const editor = useSlate();
+    console.log(props);
     const { element } = props;
+    const { focused = undefined } = element;
 
     const path = ReactEditor.findPath(editor, element);
+
     const { showAnswers, checkExercise } = getExerciseBlockProps(editor, path)
 
     const textContent = Node.string(element);
@@ -31,6 +40,11 @@ const WordOrderPractice = props => {
     const correctAnswers = getDivisions(textContent, { shuffle: false });
 
     function handleOnDragEnd(result) {
+
+        if (focused) {
+            Transforms.setNodes(editor, { focused: undefined }, { at: path });
+        }
+
         // `destination` is `undefined` if the item was dropped outside the list
         // In this case, do nothing
         if (!result.destination) {
@@ -43,13 +57,12 @@ const WordOrderPractice = props => {
             result.destination.index
         );
 
-        const path = ReactEditor.findPath(editor, element);
         Transforms.setNodes(editor, { userAnswers: checkAnswers(items, correctAnswers) }, { at: path })
     }
 
     if (showAnswers) {
         return (
-            <Flex flexWrap="wrap" alignItems="center" my="4" p="4" borderRadius="base" border="2px var(--chakra-colors-purple-600) solid">
+            <Flex flexWrap="wrap" alignItems="center" my="4" p="4" borderRadius="base" border="2px var(--chakra-colors-brand-600) solid">
                 {
                     correctAnswers.map((word) => (
                         <WordComponent key={word.id} {...{ ...word, isCorrect: true, }} />
@@ -62,18 +75,46 @@ const WordOrderPractice = props => {
     return (
         <DragDropContext onDragEnd={handleOnDragEnd} >
             <Droppable droppableId="droppable" direction="horizontal">
-                {(provided, snapshot) => (
-                    <Flex flexWrap="wrap" alignItems="center" my="4" p="4" borderRadius="base" border="2px var(--chakra-colors-purple-600) solid"
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                        className={`${snapshot.isDraggingOver && 'draggingOver'}`}
-                    >
-                        {userAnswers.map((word, index) => (
-                            <DraggableWordComponent key={word.id} {...{ ...word, index, checkExercise }} />
-                        ))}
-                        {provided.placeholder}
-                    </Flex>
-                )}
+                {(provided, snapshot) => {
+                    if (snapshot.isDraggingOver && !focused) {
+                        Transforms.setNodes(editor, {
+                            focused: {
+                                username: socket.auth.username,
+                                userID: socket.id
+                            }
+                        }, {
+                            at: path
+                        })
+                    };
+                    return (
+                        focused && focused.userID !== socket.id ? (
+                            <Tooltip label={<UserLabel message={`${focused.username} esta contestando este ejercicio`} />}>
+                                <Flex position="relative" flexWrap="wrap" alignItems="center" my="4" borderRadius="base" border="2px var(--chakra-colors-brand-600) solid"
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}
+                                    className={`${snapshot.isDraggingOver && 'draggingOver'}`}
+                                >
+                                    <Text as="span" w="3" h="3" bg="brand.500" position="absolute" transform="translateY(-50%) translateX(50%)" animation="pulse 2s infinite" boxShadow="0 0 0 #3c37cfb7" top="0" right="0" borderRadius="50%" zIndex="overlay" />
+                                    {userAnswers.map((word, index) => (
+                                        <DraggableWordComponent key={word.id} {...{ ...word, index, focused, checkExercise }} />
+                                    ))}
+                                    {provided.placeholder}
+                                </Flex>
+                            </Tooltip>
+                        ) : (
+                            <Flex position="relative" flexWrap="wrap" alignItems="center" my="4" p="4" borderRadius="base" border="2px var(--chakra-colors-brand-600) solid"
+                                ref={provided.innerRef}
+                                {...provided.droppableProps}
+                                className={`${snapshot.isDraggingOver && 'draggingOver'}`}
+                            >
+                                {userAnswers.map((word, index) => (
+                                    <DraggableWordComponent key={word.id} {...{ ...word, index, checkExercise }} />
+                                ))}
+                                {provided.placeholder}
+                            </Flex>
+                        )
+                    )
+                }}
             </Droppable>
         </DragDropContext>
     )
@@ -97,12 +138,12 @@ const WordComponent = (props) => {
     )
 }
 
-const DraggableWordComponent = ({ id, word, index, checkExercise, isCorrect }) => {
+const DraggableWordComponent = ({ id, word, index, checkExercise, focused, isCorrect }) => {
 
     function getBg(snapshot) {
 
         if (snapshot.isDragging) {
-            return "purple.600"
+            return "brand.300"
         }
 
         if (checkExercise) {
@@ -112,7 +153,7 @@ const DraggableWordComponent = ({ id, word, index, checkExercise, isCorrect }) =
             return "yellow.300"
         }
 
-        return "purple.500"
+        return "brand.500"
     }
 
     function getColor() {
@@ -123,7 +164,7 @@ const DraggableWordComponent = ({ id, word, index, checkExercise, isCorrect }) =
     }
 
     return (
-        <Draggable key={id} draggableId={id} index={index}>
+        <Draggable isDragDisabled={!!focused} key={id} draggableId={id} index={index}>
             {(provided, snapshot) => (
                 <Text
                     userSelect="none"
@@ -132,7 +173,7 @@ const DraggableWordComponent = ({ id, word, index, checkExercise, isCorrect }) =
                     py="1"
                     borderRadius="base"
                     as="span"
-                    cursor="move"
+                    cursor={!!focused ? "not-allowed" : "move"}
                     fontWeight="medium"
                     color={getColor()}
                     transition="all ease 300"
