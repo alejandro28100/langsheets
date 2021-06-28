b229f99b1a60fcde5c365aade55c0473f5161f28
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-06-28 00:21:32 -0500

    Clean Mark

--- src/components/Slate/Inline/MissingWord/Mark.js
+++ src/components/Slate/Inline/MissingWord/Mark.js
@@ -1,16 +1,6 @@
 import { FaCheck, FaTimes } from "react-icons/fa"
 import { Icon } from "@chakra-ui/icon";
 import { InputRightElement } from "@chakra-ui/input";
-// const Mark = ({ isCorrect, checkExercise, showAnswers }) => {
-//     //if the exercises is not checked or the answers are shown, render nothing
-//     if (!checkExercise || showAnswers) return null;
-//     //render correct mark
-//     if (isCorrect) {
-//         return <InputRightElement as="span" children={<Icon as={FaCheck} color="green.400" />} />
-//     }
-//     //render wrong mark
-//     return <InputRightElement as="span" children={<Icon as={FaTimes} color="red.400" />} />
-// }
 
 const Mark = ({ show, type }) => {
     if (!show) return null;
